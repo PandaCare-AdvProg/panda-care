7493cb373786d447385dfcbffea54a2d92afe2de
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2025-03-25 13:15:32 +0700

    initial codebase push

---
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/PandacareApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.pandacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandacareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandacareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/pandacare/PandacareApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.pandacare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandacareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
