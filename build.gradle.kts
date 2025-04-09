44393f1b8629fe8da12a44f4ac270d5b106667ba
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2025-04-09 13:05:18 +0700

    [REFACTOR] adding configuration for jwt authentication (not finished)  and adding the reuqiured dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation ("jakarta.persistence:jakarta.persistence-api:3.1.0")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+
 }
 
 tasks.test {
