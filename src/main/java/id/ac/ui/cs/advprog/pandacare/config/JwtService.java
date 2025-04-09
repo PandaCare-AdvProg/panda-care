8e307edbbddd7185578fa40a673788bc2fb52757
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2025-04-09 13:05:18 +0700

    [REFACTOR] adding configuration for jwt authentication (not finished)  and adding the reuqiured dependencies

---
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/config/JwtService.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.pandacare.config;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String jwt) {
+        return null;
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+
+    }
+}
