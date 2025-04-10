1a6976a90d33beb3d8adceeeba5dc5921277da72
caqqmarina <chiara.aqmarina@ui.ac.id>
2025-04-10 08:46:38 +0700

    [Red] Build failure due to unresolved symbols (Lombok/Spring annotations)

---
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/Auth/AuthenticationService.java
@@ -0,0 +1,45 @@
+package id.ac.ui.cs.advprog.pandacare.Auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user =  User.builder()
+            .firstname(request.getFirstname())
+            .lastname(request.getLastname())
+            .email(request.getEmail())
+            .password(passwordEncoder.encode(request.getPassword()))
+            .role(Role.USER)
+            .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return authenticationResponse.builder()
+            .token(jwtToken)
+            .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+            )
+        );
+        var user = repository.findByEmail(request.getEmail())
+            .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return authenticationResponse.builder()
+            .token(jwtToken)
+            .build();
+        return null;
+    }
+
+}
