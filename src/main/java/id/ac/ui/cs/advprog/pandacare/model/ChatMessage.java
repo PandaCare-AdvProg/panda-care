c9d04c18c2c9357afb62760be11b8aa0229bb8b2
KeiraDiaz <nkeiradiaz@gmail.com>
2025-04-11 02:48:29 +0700

    [RED] Added skeleton for ChatMessage model

--- src/main/java/id/ac/ui/cs/advprog/pandacare/model/ChatMessage.java
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/model/ChatMessage.java
@@ -1,4 +1,20 @@
 package id.ac.ui.cs.advprog.pandacare.model;
 
+import id.ac.ui.cs.advprog.pandacare.Auth.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "chat_message")
 public class ChatMessage {
+
+
+    // Getters and Setters
 }
+
