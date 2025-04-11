52792856dd5e2a7bb2e8028b6c82c951fd5c2ae8
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2025-04-11 19:07:54 +0700

    [REFATOR] fixing the database so that it is not confused anymore and it can be built and fixing the test

--- src/main/java/id/ac/ui/cs/advprog/pandacare/Auth/User.java
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/Auth/User.java
@@ -13,6 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED) 
 @Table(name = "_user")
 public class User implements UserDetails {
     @Id
