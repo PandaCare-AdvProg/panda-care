3bfff6508a6d5bbc561c2c4da2770622be88904a
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2025-05-01 18:33:14 +0700

    [GREEN] implemented ScheduleRepository

---
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/repository/ScheduleRepository.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.pandacare.repository;
+
+import id.ac.ui.cs.advprog.pandacare.model.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    void deleteById(Long scheduleId);
+    List<Schedule> findByDoctorId(Long doctorId);
+    Optional<Schedule> findById(Long scheduleId);
+    List<Schedule> findByDayOfWeek(String dayOfWeek);    
+}
