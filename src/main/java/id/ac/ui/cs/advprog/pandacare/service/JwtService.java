ec0e16f426083dddf1b9499f19eda9c1dc192222
KeiraDiaz <nkeiradiaz@gmail.com>
2025-05-07 16:18:50 +0700

    [RED] still has issues with secrets, tbc

--- src/main/java/id/ac/ui/cs/advprog/pandacare/service/JwtService.java
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/service/JwtService.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import java.security.Key;
@@ -15,10 +14,8 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-
-    @Value("${JWT_SECRET_KEY}")
-    private String SECRET_KEY;
-
+    
+    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
     private static final long jwtExpiration = 1000 * 60 * 60 * 10; 
     private static final long refreshExpiration = 1000 * 60 * 60 * 24 * 30;
     
