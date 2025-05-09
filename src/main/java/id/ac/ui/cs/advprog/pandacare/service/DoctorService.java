854b5b8dc9ae97f072a221aa299af3745dc08a48
skibidiyo <diyobanu@gmail.com>
2025-05-09 11:15:46 +0700

    [GREEN] implemented doctor servie

--- src/main/java/id/ac/ui/cs/advprog/pandacare/service/DoctorService.java
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/service/DoctorService.java
@@ -1,4 +1,15 @@
 package id.ac.ui.cs.advprog.pandacare.service;
 
-public class DoctorService {
+import java.util.List;
+import java.util.Optional;
+import id.ac.ui.cs.advprog.pandacare.model.Doctor;
+
+public interface DoctorService {
+    List<Doctor> getAllDoctors();
+    Optional<Doctor> getDoctorById(Long id);
+    Doctor createDoctor(Doctor doctor);
+    Doctor updateDoctor(Long id, Doctor doctor);
+    void deleteDoctor(Long id);
+    List<Doctor> searchByName(String name);
+    List<Doctor> searchBySpecialty(String specialty);
 }
