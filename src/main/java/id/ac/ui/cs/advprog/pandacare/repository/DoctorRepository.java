cc372c074e27b1c2f5205d44ecbebfde2c7a10e6
skibidiyo <diyobanu@gmail.com>
2025-05-09 11:09:17 +0700

    [RED] create tests for doctor repository

--- src/main/java/id/ac/ui/cs/advprog/pandacare/repository/DoctorRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/repository/DoctorRepository.java
@@ -1,24 +1,10 @@
 package id.ac.ui.cs.advprog.pandacare.repository;
 
-import id.ac.ui.cs.advprog.pandacare.dto.DoctorDTO;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import id.ac.ui.cs.advprog.pandacare.model.Doctor;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import java.util.Optional;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-
-    @Query("select new id.ac.ui.cs.advprog.pandacare.dto.DoctorDTO(d.id, d.specialty, d.workingAddress) " +
-           "from Doctor d where d.id = :id")
-    Optional<DoctorDTO> findDoctorDTOById(@Param("id") Long id);
-
-    @Query("select new id.ac.ui.cs.advprog.pandacare.dto.DoctorDTO(d.id, d.specialty, d.workingAddress) "
-    + "from Doctor d where d.email = :email")
-    Optional<DoctorDTO> findDoctorDTOByEmail(@Param("email") String email);
-    
-    @Query("select d from Doctor d where d.email = :email")
-    Optional<Doctor> findDoctorByEmail(@Param("email") String email);
-
+public interface DoctorRepository1 extends JpaRepository<Doctor, Long> {
+    List<Doctor> findByNameContainingIgnoreCase(String name);
+    List<Doctor> findBySpecialtyContainingIgnoreCase(String specialty);
 }
