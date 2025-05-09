80ada1ed3ee0bdee7057688dde09617e6e6d0a4a
KeiraDiaz <nkeiradiaz@gmail.com>
2025-05-09 13:39:26 +0700

    [GREEN] implemented Api Response

---
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/dto/ApiResponse.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.pandacare.dto;
+
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+}
