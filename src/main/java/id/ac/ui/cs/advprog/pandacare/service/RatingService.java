d41c08c7cdcd91da277621beb53ab4f850dbcc62
unknown <Chiara@SKUSUMO-X30W-J.id.oracle.com>
2025-05-11 21:10:46 +0700

    [REFACTOR] fix duplicates in RatingService

--- src/main/java/id/ac/ui/cs/advprog/pandacare/service/RatingService.java
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/service/RatingService.java
@@ -1,10 +1,5 @@
 package id.ac.ui.cs.advprog.pandacare.service;
 
-public class RatingService {
-
-}
-package id.ac.ui.cs.advprog.pandacare.service;
-
 import id.ac.ui.cs.advprog.pandacare.model.Consultation;
 import id.ac.ui.cs.advprog.pandacare.model.Doctor;
 import id.ac.ui.cs.advprog.pandacare.model.Patient;
