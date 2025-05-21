9ba84911d72dbb391810dc789391beb67df212b9
KeiraDiaz <nkeiradiaz@gmail.com>
2025-05-21 18:06:33 +0700

    [REFACTOR] simplified ChatMessage model, modif db, and test to properly test out ws

--- src/main/java/id/ac/ui/cs/advprog/pandacare/repository/ChatMessageRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/repository/ChatMessageRepository.java
@@ -1,17 +1,14 @@
 package id.ac.ui.cs.advprog.pandacare.repository;
 
-import id.ac.ui.cs.advprog.pandacare.model.ChatMessage;
-import id.ac.ui.cs.advprog.pandacare.model.ChatRoom;
-import id.ac.ui.cs.advprog.pandacare.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import id.ac.ui.cs.advprog.pandacare.model.ChatMessage;
 
 import java.util.List;
 
 @Repository
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    List<ChatMessage> findByRoomOrderByCreatedAtAsc(ChatRoom room);
-    List<ChatMessage> findByRoomAndDeletedFalseOrderByCreatedAtAsc(ChatRoom room);
-    List<ChatMessage> findBySender(User sender);
-    List<ChatMessage> findByReceiver(User receiver);
-}
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, String> {
+    
+    List<ChatMessage> findByChatRoomRoomId(String roomId);
+} 
