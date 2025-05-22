6e886438bc4961b128e9c81ad1faf920d554dd97
skibidiyo <diyobanu@gmail.com>
2025-05-22 22:42:15 +0700

    [REFACTOR] Making the HTML and fixing the search filter

--- src/main/java/id/ac/ui/cs/advprog/pandacare/service/DoctorService.java
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/service/DoctorService.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.pandacare.service;
 
+import java.time.DayOfWeek;
 import java.util.List;
 import java.util.Optional;
 import id.ac.ui.cs.advprog.pandacare.model.Doctor;
@@ -12,4 +13,6 @@ public interface DoctorService {
     void deleteDoctor(Long id);
     List<Doctor> searchByName(String name);
     List<Doctor> searchBySpecialty(String specialty);
+    List<Doctor> searchByScheduleDay(DayOfWeek day);
+    List<Doctor> searchDoctors(String name, String specialty, DayOfWeek day);
 }
