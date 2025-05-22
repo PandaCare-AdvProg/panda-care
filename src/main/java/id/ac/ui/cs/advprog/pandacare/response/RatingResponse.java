b8f716563a8fc11210f1248ba39af681f91334a9
unknown <Chiara@SKUSUMO-X30W-J.id.oracle.com>
2025-05-22 23:08:21 +0700

    [Refactor] changes in the repository

--- src/main/java/id/ac/ui/cs/advprog/pandacare/response/RatingResponse.java
+++ src/main/java/id/ac/ui/cs/advprog/pandacare/response/RatingResponse.java
@@ -1,5 +1,21 @@
 package id.ac.ui.cs.advprog.pandacare.response;
 
-public class RatingResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RatingResponse {
+    private Long id;
+    private Long doctorId;
+    private String doctorName;
+    private Long patientId;
+    private String patientName;
+    private Long consultationId;
+    private int score;
+    private String review;
 }
